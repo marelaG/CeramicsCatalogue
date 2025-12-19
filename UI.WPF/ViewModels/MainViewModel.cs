004c7200f3c0142546bfaf20925083842b6b73fd
marelaG <marcelina.gancewska@student.kuleuven.be>
2025-12-19 16:23:14 +0100

    Add buttons

--- UI.WPF/ViewModels/MainViewModel.cs
+++ UI.WPF/ViewModels/MainViewModel.cs
@@ -8,6 +8,7 @@ namespace GancewskaKerebinska.CeramicsCatalogue.UI.WPF.ViewModels
         private object _currentView;
         private readonly ProductViewModel _productViewModel;
         private readonly ProducerViewModel _producerViewModel;
+        private string _searchText;
 
         public object CurrentView
         {
@@ -19,13 +20,31 @@ namespace GancewskaKerebinska.CeramicsCatalogue.UI.WPF.ViewModels
             }
         }
 
+        public string SearchText
+        {
+            get => _searchText;
+            set
+            {
+                _searchText = value;
+                OnPropertyChanged();
+            }
+        }
+
         public ICommand ChangeViewCommand { get; }
+        public ICommand AddCommand { get; }
+        public ICommand ModifyCommand { get; }
+        public ICommand DeleteCommand { get; }
+        public ICommand SearchCommand { get; }
 
         public MainViewModel()
         {
             _productViewModel = new ProductViewModel();
             _producerViewModel = new ProducerViewModel();
             ChangeViewCommand = new RelayCommand(ChangeView);
+            AddCommand = new RelayCommand(Add);
+            ModifyCommand = new RelayCommand(Modify);
+            DeleteCommand = new RelayCommand(Delete);
+            SearchCommand = new RelayCommand(Search);
 
             // Set the initial view
             CurrentView = _productViewModel;
@@ -41,6 +60,28 @@ namespace GancewskaKerebinska.CeramicsCatalogue.UI.WPF.ViewModels
             {
                 CurrentView = _producerViewModel;
             }
+        }
+
+        private void Add(object parameter)
+        {
+            // TODO: Implement Add functionality
+        }
+
+        private void Modify(object parameter)
+        {
+            // TODO: Implement Modify functionality
+        }
+
+
+
+        private void Delete(object parameter)
+        {
+            // TODO: Implement Delete functionality
+        }
+
+        private void Search(object parameter)
+        {
+            // TODO: Implement Search functionality
         }
     }
 }
